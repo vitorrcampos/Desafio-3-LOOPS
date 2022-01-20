014f7837e02b348e691d738b63b303ae0ff2bd57
joao vitor <jaun.campos04@gmail.com>
2022-01-20 15:10:15 -0300

    Desafio 3 concluido.

---
+++ script.js
@@ -0,0 +1,43 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+console.log("Criar um loop que conte de 1 até 10 usando FOR")
+
+for(let contador = 1; 10 >= contador; contador += 1){
+    console.log(contador)
+}
+
+
+
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+console.log("Criar um loop que conte de 10 até 1 usando WHILE")
+
+let contador = 10
+
+while (1 <= contador){
+    console.log(contador)
+    contador -=1
+}
+
+
+
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+console.log("Criar um loop que conte todos os números ímpares de 1 até 100")
+
+for(let contador = 1; 100 >= contador; contador += 2){
+    console.log(contador)
+}
+
+
+
+
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+console.log("Criar um loop que conte todos os números pares de 0 a 100")
+
+for(let contador = 2; 100 >= contador; contador += 2){
+    console.log(contador)}
